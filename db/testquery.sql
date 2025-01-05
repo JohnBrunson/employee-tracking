cf272dc003ace6781f04714b7bc845fb1e6692ac
JohnBrunson <JohnFBrunson@gmail.com>
2025-01-04 20:09:09 -0800

    View All Employees hangs, other view options work ok.

---
+++ db/testquery.sql
@@ -0,0 +1,5 @@
+SELECT employee.id, employee.first_name AS "Employee First Name", employee.last_name AS "Employee Last Name", role.title, department.name AS Department, role.salary, CONCAT (manager.first_name, ' ', manager.last_name) AS "Manager Name" 
+FROM employee
+INNER JOIN role ON employee.role_id = role.id
+INNER JOIN department ON role.department_id = department.id 
+LEFT JOIN employee AS manager ON employee.manager_id = manager.id;
